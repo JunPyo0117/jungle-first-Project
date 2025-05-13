5cbed9578a7cf75ec4c3b6551c61f10aac9106f6
sunha20 <c.seonha20@gmail.com>
2025-05-13 17:17:19 +0900

    db fix

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ MONGO_URI = "mongodb://localhost:27017"
 
 # MongoDB 연결
 client = MongoClient(MONGO_URI)
-db = client['user_db']
+db = client['cs_paper']
 users = db['users']
+questions = db['questions']
+answers = db['answers']
 
 # 회원가입
 @app.route('/register', methods=['GET', 'POST'])
